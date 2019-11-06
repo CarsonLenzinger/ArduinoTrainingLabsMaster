babd786d4363cf22151914fd43299c6d46382477
Carson <02carson.lenzinger@gulllakecs.org>
2019-11-06 12:58:44 -0500

    Added Blink

---
+++ sketch_nov06a/Lab1_Blink.ino
@@ -0,0 +1,21 @@
+/*Programmer: Carson Lenzinger
+ * Date: 11.6.19
+ * Program: Blink
+ * 
+ * This program will turn on an LED for specified amounts of time, turn it off, and back on in a While Loop.
+ */
+
+//initialize our led to Pin 13
+int led = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //Using the Function pinMode to call led and provide Output
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
