eee9a35038d23d4b59c4f908fe848d62aad1b440
Carson <02carson.lenzinger@gulllakecs.org>
2019-12-03 13:52:03 -0500

    Add if - else for Led to go on and off based on buttonState

--- sketch_nov06a/Lab1_Blink/Lab1_Blink.ino
+++ sketch_nov06a/Lab1_Blink/Lab1_Blink.ino
@@ -1,16 +1,16 @@
 /*Programmer: Carson Lenzinger
- * Date: 11.6.19
- * Program: Blink
- * 
- * This program will turn on an LED for specified amounts of time, turn it off, and back on in a While Loop.
- * 
- * Programmer: Carson Lenzinger
- * Date: 11.6.19
- * Program: digitalRead() & the Serial Port
- * 
- * Reads a digital input on pin2, prints the results to the serial monitor.
- *
- */
+   Date: 11.6.19
+   Program: Blink
+
+   This program will turn on an LED for specified amounts of time, turn it off, and back on in a While Loop.
+
+   Programmer: Carson Lenzinger
+   Date: 11.6.19
+   Program: digitalRead() & the Serial Port
+
+   Reads a digital input on pin2, prints the results to the serial monitor.
+
+*/
 
 //initialize our led to Pin 13
 int led = 13;
@@ -24,25 +24,22 @@ void setup() {
   pinMode(led, OUTPUT);
 
   //initializes serial communication at 9600 bits per second
- Serial.begin(9600);
- //make pushButton pin an input
- pinMode(pushButton, INPUT);
+  Serial.begin(9600);
+  //make pushButton pin an input
+  pinMode(pushButton, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int buttonState = digitalRead(pushButton);
   //print out the state of the button
   Serial.println(buttonState);
-  if (buttonState==HIGH){
-  digitalWrite(led, HIGH);
-  delay(1500);
-  digitalWrite(led, LOW);
-  delay(500);
+  if (buttonState == HIGH) {
+    digitalWrite(led, LOW);
   }
-  //read the input pin
 
-// print out the state of the button
-Serial.println(buttonState);
-delay(250);  // delay in between readings for stability
+  else {
+    digitalWrite(led, HIGH);
   }
+
+  delay(1);  // delay in between readings for stability
+}
